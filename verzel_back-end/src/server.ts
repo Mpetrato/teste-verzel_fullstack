dd1a5e8b4c0e08fc0a9987272d49ddbdffef2dd2
Matheus Petrato <matheuspetratomp@gmail.com>
2022-10-07 04:48:54 -0300

    finish api

--- verzel_back-end/src/server.ts
+++ verzel_back-end/src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser';
+import authRoutes from './routes/auth'
+import carRoutes from './routes/cars'
 
 
 
@@ -11,6 +13,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
+
+app.use('/api/auth', authRoutes)
+app.use('/api/cars', carRoutes)
 
 
 app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`))
